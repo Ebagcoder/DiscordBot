617551bfec0e7e3ca64c7f3e191a7d5a2dba74b8
Ebagcoder <56970107+Ebagcoder@users.noreply.github.com>
2020-08-16 21:58:32 -0400

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
+const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); }); 
 exports.run = (client, message, args) => {
     const command = message.content.split(' '); 
 
     if(command[1] == undefined){
         const embed = new Discord.MessageEmbed()
-            .setColor("#20b1d0")
+            .setColor(randomColor)
             .addField("Commands",`You can view commands at [github.com](https://github.com/Ebagcoder/DiscordBot/wiki/Bot-Commands)`);
         message.channel.send(embed);    
     }
@@ -19,7 +20,7 @@ exports.run = (client, message, args) => {
 
             try{
                 const embed = new Discord.MessageEmbed()
-                    .setColor("##20b1d0")
+                    .setColor(randomColor)
                     .addField(`Command: $${helpMe[commandName].name}`,`Group: ${helpMe[commandName].group}\n`+`Desc: ${helpMe[commandName].desc}\n`+`Usage: ${helpMe[commandName].usage}\n`+`Example: ${helpMe[commandName].example}\n`);
                 message.channel.send(embed);    
             }
